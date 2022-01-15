144e1063d77ae313ecbd2965b53cc6f5b92bca5c
yasir2000 <yasirkaram@gmail.com>
2022-01-15 21:19:16 +0300

    fixed notifier.js

--- notifier/notifier.js
+++ notifier/notifier.js
@@ -1,34 +1,32 @@
-const notifier = require('../index');
 const path = require('path');
+const notifier = require('node-notifier');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 9000;
 
-notifier.notify(
-  {
-    message: 'Are you sure you want to continue?',
-    icon: path.join(__dirname, 'coulson.jpg'),
-    actions: ['OK', 'Cancel']
-  },
-  (err, data) => {
-    // Will also wait until notification is closed.
-    console.log('Waited');
-    console.log(JSON.stringify({ err, data }, null, '\t'));
-  }
-);
-
-// Built-in actions:
-notifier.on('timeout', () => {
-  console.log('Timed out!');
-});
-notifier.on('activate', () => {
-  console.log('Clicked!');
-});
-notifier.on('dismissed', () => {
-  console.log('Dismissed!');
+app.use(bodyParser.json());
+app.get('/health', (req, res) => res.status(200).send());
+app.post('/notify', (req, res) => {
+    notify(req.body, reply => res.send(reply))
 });
 
-// Buttons actions (lower-case):
-notifier.on('ok', () => {
-  console.log('"Ok" was pressed');
-});
-notifier.on('cancel', () => {
-  console.log('"Cancel" was pressed');
-});
+app.listen(port, () => console.log(`server is up and running on port: ${port}`));
+
+const notify = ({title, message}, cb) => {
+    notifier.notify(
+        {
+            title: title || 'Unknown title',
+            message: message || 'Unknown message',
+            icon: path.join(__dirname, 'gophertuts.png'),
+            sound: true,
+            wait: true,
+            reply: true,
+            closeLabel: 'Completed?',
+            timeout: 15,
+        },
+        (err, response, reply) => {
+            cb(reply)
+        }
+    );
+};
